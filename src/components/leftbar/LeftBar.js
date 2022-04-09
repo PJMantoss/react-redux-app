4131b1213e49e666507c7dcbb834b4e5ba14f943
PJMantoss <joelptoss@gmail.com>
2022-04-09 18:50:10 +0100

    update LeftBar component

--- src/components/leftbar/LeftBar.js
+++ src/components/leftbar/LeftBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import FileCopyOutlinedIcon from '@mui/icons-material/FileCopyOutlined';
 
 const LeftBar = () => {
   return (
