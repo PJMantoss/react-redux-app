8bae43ab3d9766decfe8c5ed4dbce8fe97f4732e
PJMantoss <joelptoss@gmail.com>
2022-04-09 19:09:49 +0100

    update MenuLink

--- src/components/menuLink/MenuLink.js
+++ src/components/menuLink/MenuLink.js
@@ -3,8 +3,10 @@ import React from 'react'
 const MenuLink = ({ icon, text }) => {
   return (
     <div className='menulink'>
-        <span className='menuLinkText'></span>
-        <span className='menuLinkTextName'></span>
+        <span className='menuLinkText'> {} </span>
+        <span className='menuLinkTextName'>
+            {}
+        </span>
     </div>
   )
 }
