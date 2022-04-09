6334478ec1f2c680a652e0a6f54dedfd20411bde
PJMantoss <joelptoss@gmail.com>
2022-04-09 18:59:54 +0100

    update LeftBar component

--- src/components/leftbar/LeftBar.js
+++ src/components/leftbar/LeftBar.js
@@ -7,9 +7,12 @@ import {
     HomeOutlinedIcon,
     ListOutlinedIcon,
     MovieCreationOutlinedIcon,
-    
+
 
 } from '@mui/icons-material';
+import PhotoSizeSelectActualOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActualOutlined';
+import ScheduleOutlinedIcon from '@mui/icons-material/ScheduleOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 const LeftBar = () => {
   return (
