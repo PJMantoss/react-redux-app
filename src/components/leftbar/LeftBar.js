f822af6d581b1c573dc058ddfb32c8260dbc0374
PJMantoss <joelptoss@gmail.com>
2022-04-09 19:23:27 +0100

    update LeftBar

--- src/components/leftbar/LeftBar.js
+++ src/components/leftbar/LeftBar.js
@@ -21,15 +21,15 @@ const LeftBar = () => {
         <div className='leftbarWrapper'>
             <MenuLink icon={<HomeOutlinedIcon />} text='Homepage' />
             <MenuLink icon={<ListOutlinedIcon />} text='Lists' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
-            <MenuLink icon={} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
+            <MenuLink icon={< />} text='' />
         </div>
     </div>
   )
