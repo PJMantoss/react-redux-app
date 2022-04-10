8a587ef69da09d6f972a1391b95de2d5b6df94f0
PJMantoss <joelptoss@gmail.com>
2022-04-10 17:36:59 +0100

    update Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = () => {
                 <span className='navbarLink'>About</span>
                 <span className='navbarLink'>Contact</span>
             </div>
+            <div className='navarCenter'></div>
         </div>
     </div>
   )
