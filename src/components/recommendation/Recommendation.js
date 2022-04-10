ee58929974968e058eba43fc442f3fc156baaac1
PJMantoss <joelptoss@gmail.com>
2022-04-10 18:00:29 +0100

    update Recommendation

--- src/components/recommendation/Recommendation.js
+++ src/components/recommendation/Recommendation.js
@@ -6,9 +6,13 @@ const Recommendation = ({ type }) => {
            ? "Recommended for Joel" 
            : title = 'popular'
            ? "Popular on Chapolmi"
-           : "Editor's Choice"
+           : "Editor's Choice";
 
-    const img = type
+    const img = type === 'user' 
+            ? 'https://images.pexels.com/photos/5797991/pexels-photo-5797991.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' 
+            : type === 'popular'
+            ? 'https://images.pexels.com/photos/5191390/pexels-photo-5191390.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+            : 'https://images.pexels.com/photos/2733659/pexels-photo-2733659.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
   return (
     <div className='recommendation'></div>
   )
