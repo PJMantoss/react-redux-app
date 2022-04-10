d1d41c98d4b583cc27248d54507bb076d9b30012
PJMantoss <joelptoss@gmail.com>
2022-04-10 18:46:48 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import LeftBar from './components/leftbar/LeftBar';
 import Navbar from './components/navbar/Navbar';
+import Update from './components/update/Update';
+import Rightbar from './components/rightbar/Rightbar';
 import './App.css';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <>
       <Navbar />
       <LeftBar />
+      <Update />
+      <Rightbar />
     </>
   );
 }
