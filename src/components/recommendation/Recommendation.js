b7a5b1ae309bf6c511605f0391a4f4f4c502f58d
PJMantoss <joelptoss@gmail.com>
2022-04-10 17:55:43 +0100

    update Recommendation

--- src/components/recommendation/Recommendation.js
+++ src/components/recommendation/Recommendation.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Recommendation.css'
 
 const Recommendation = ({ type }) => {
+    const title = type
+
+    const img = type
   return (
     <div className='recommendation'></div>
   )
