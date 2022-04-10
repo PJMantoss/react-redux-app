e54d27e8c0a1f7fa1fd73bd90bb53f187a2d52f6
PJMantoss <joelptoss@gmail.com>
2022-04-10 18:22:01 +0100

    update Update

--- src/components/update/Update.js
+++ src/components/update/Update.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import Warning from '../warning/Warning';
+import './Warning.css';
 
 const Update = () => {
+    const [] = useState('');
+    const [] = useState('');
+    
   return (
     <div>Update</div>
   )
