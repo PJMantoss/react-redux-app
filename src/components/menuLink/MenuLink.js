a46a28aac406161c6c045b505b3638f989858b8b
PJMantoss <joelptoss@gmail.com>
2022-04-11 17:56:15 +0100

    update menuLink

--- src/components/menuLink/MenuLink.js
+++ src/components/menuLink/MenuLink.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './MenuLink.css';
 
 const MenuLink = ({ icon, text }) => {
+  const name = useSelector(state => state.user.name);
+
   return (
     <div className='menulink'>
         <span className='menuLinkText'> {text} </span>
         <span className='menuLinkTextName'>
-            {text === 'Logout' && '( Joel )'}
+            {text === 'Logout' && `( ${name} )`}
         </span>
     </div>
   )
