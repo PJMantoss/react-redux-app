95be6b88ffe5fadeab549990a280cbd5bda82c36
PJMantoss <joelptoss@gmail.com>
2022-04-12 10:50:08 +0100

    update Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,6 +6,8 @@ import './Navbar.css'
 const Navbar = () => {
     const name = useSelector(state => state.user.name);
 
+    console.log("navbar re-rendered")
+
   return (
     <div className='navbar'>
         <div className='navbarWrapper'>
