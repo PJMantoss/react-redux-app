2131e92272c8dd84b4d3062207e5ff4c7cfada4e
PJMantoss <joelptoss@gmail.com>
2022-04-12 10:58:11 +0100

    update userSlice

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -12,9 +12,7 @@ export const userSlice = createSlice({
             state.name = action.payload.name;
             state.email = action.payload.email;
         },
-        remove: (state) => {
-            state = null;
-        }
+        remove: (state) => (state = {})
     }
 });
 
